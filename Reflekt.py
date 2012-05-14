ec72875b22531bb8fed1e03e814430b0c0dff2da
likang <likang@kisbear.com>
2012-05-14 23:31:28 +0800

    tmp

--- Reflekt.py
+++ Reflekt.py
@@ -5,17 +5,17 @@ import random
 import time
 from pygame.locals import *
 
-BOX_SIDE    = 100
+IMG_SIDE    = 100
 MARK_SIDE   = 20
-PADDING     = 20
+PADDING     = 10
 ROWS        = 4
 COLS        = 6
-MOVE_SPEED  = 8 # BOX_SIDE + PADDING must be divided by SPPED exactly
-SCALE_SPEED = 10
+MOVE_SPEED  = 8     # BOX_SIDE must be divided by MOVE_SPPED exactly
+SCALE_SPEED = 10    # 
+BOX_SIDE    = IMG_SIDE + PADDING*2
 IN_MIRROR   = False
-BLOCK_SIDE  = BOX_SIDE + PADDING
 
-BG_COLOR   = (0,0,0)
+BG_COLOR    = (0,0,0)
 COLORS = {
     "GREEN"     : (161,234,100),
     "RED"       : (229,75 ,99 ),
@@ -24,11 +24,6 @@ COLORS = {
     "BLUE"      : (64 ,178,223),
     "LIGHTGREEN": (237,254,235),
     }
-cmds = []
-boxs =[]
-mirror_colors=[]
-animations=[]
-marked_index = -1
 
 class Box(pygame.sprite.Sprite):
     """pure color block as sprite which has two states: marked and unmarked"""
@@ -48,41 +43,37 @@ class Box(pygame.sprite.Sprite):
         self.rect = self.rect.move(vector)
 
 class Animation():
-    """basic animation unit, an animation will be started only when it's parent is finished"""
+    """basic animation unit, it will be started after it's parent is finished"""
     def __init__(self, action, box, target = None, parent = None):
         self.action = action
         self.box    = box
         self.target = target
         self.parent = parent
         self.finished  = False
-        self.first_run = True 
 
     def update(self):
-        if(self.action == "move"):
+        if self.action == "move" :
             topleft = self.box.rect.topleft
-            if self.first_run:
-                self.target = (topleft[0] + self.target[0]*BLOCK_SIDE, topleft[1] + self.target[1]*BLOCK_SIDE)
             if not topleft == self.target:
                 x_speed = MOVE_SPEED*cmp(self.target[0] - topleft[0], 0)
                 y_speed = MOVE_SPEED*cmp(self.target[1] - topleft[1], 0)
                 self.box.move((x_speed,y_speed))
             if self.box.rect.topleft == self.target:
                 self.finished = True
-                global boxs
-                boxs[self.target[0]/BLOCK_SIDE + COLS*(self.target[1]/BLOCK_SIDE)] = self.box
-        elif(self.action == "renew"):
+        elif self.action == "renew" :
             box_side = self.box.image.get_size()[0]
             if not box_side == 0:
                 self.box.image = pygame.transform.scale(self.box.image, (box_side - SCALE_SPEED, )*2)
                 self.box.move((SCALE_SPEED/2, )*2)
             if self.box.image.get_size()[0] == 0:
                 self.finished = True  
-                self.box.move((-(self.box.rect.left%BLOCK_SIDE)+PADDING/2, ROWS*BLOCK_SIDE+PADDING/2-self.box.rect.top))
-                self.box.image = get_surface(self.target)
-                self.box.color = self.target
+                new_color = self.target[0]
+                self.box.image = get_surface(new_color)
+                self.box.color = new_color
+                new_pos = self.target[1]
+                self.box.move((new_pos[0] - self.box.rect.left, new_pos[1] - self.box.rect.top))
         else:
             self.finished = True
-        self.first_run = False
 
 def get_random_color():
     return random.choice(COLORS.keys())
@@ -95,7 +86,10 @@ def get_surface(color, marked = False, surfaces = {}):
     surface = surfaces.get(key)
     if not surface:
         surface = pygame.Surface((BOX_SIDE, BOX_SIDE))
-        surface.fill(COLORS[color])
+        surface.fill(BG_COLOR)
+        img = pygame.Surface((IMG_SIDE, IMG_SIDE))
+        img.fill(COLORS[color])
+        surface.blit(img, (PADDING, PADDING))
         if marked:
             img_mark = pygame.Surface((MARK_SIDE,MARK_SIDE))
             img_mark.fill(BG_COLOR)
@@ -109,7 +103,7 @@ def generate_boxs():
     for y in range(0,ROWS):
         for x in range(0,COLS):
             color = get_random_color()
-            b = Box(color , (PADDING/2 + BLOCK_SIDE*x, PADDING/2 + BLOCK_SIDE*y))
+            b = Box(color , (BOX_SIDE*x, BOX_SIDE*y))
             boxs.append(b)
     return boxs
 
@@ -118,24 +112,26 @@ def generate_mirror_colors(boxs):
         colr = box.color
 
 def pos_to_index(pos):
-    index =  pos[0]/BLOCK_SIDE + COLS*(pos[1]/BLOCK_SIDE)
-    return index if index >= 0 and index < len(boxs) else -1
+    index =  pos[0]/BOX_SIDE + COLS*(pos[1]/BOX_SIDE)
+    return index if index >= 0 and index < COLS*ROWS else -1
+
+def index_to_pos(index):
+    return ((index%COLS)*BOX_SIDE, (index/COLS)*BOX_SIDE)
+
+def row_gap(you, me):
+    return you/COLS - me/COLS
+
+def col_gap(you, me):
+    return you%COLS - me%COLS
 
 def is_around(you, me):
-    if you/COLS == me/COLS and (you - me) in (-1,0,1):
-        return True
-    if (you - me) in (-COLS, COLS):
-        return True
-    return False
+    return abs(row_gap(you, me)) + abs(col_gap(you, me)) in (1,0)
 
-def get_move(start, end):
-    return (start, end%COLS - start%COLS, end/COLS - start/COLS)
-
-def click_at(pos):
-    global boxs, marked_index, cmds
+def click_at(pos, boxs, marked_index):
     clicked_index= pos_to_index(pos)
+    cmds = []
     if clicked_index == -1:
-        return
+        return cmds, marked_index
     if marked_index == -1:
         boxs[clicked_index].mark(True)
         marked_index = clicked_index
@@ -146,48 +142,57 @@ def click_at(pos):
         marked_box = boxs[marked_index]
         clicked_box = boxs[clicked_index]
         marked_box.mark(False)
-        cmds.append("1 0 move %s %s %s" % get_move(clicked_index, marked_index))
-        cmds.append("2 0 move %s %s %s" % get_move(marked_index, clicked_index))
-        cmds.append("3 1 renew %s %s" % (clicked_index, get_random_color()))
-        cmds.append("4 2 renew %s %s" % (marked_index, get_random_color()))
-        cmds.append("5 3 landslip %s %s"  % (clicked_index, marked_index))
+        cmds.append("switch %s %s" % (clicked_index, marked_index))
+        # cmds.append("3 1 renew %s %s" % (clicked_index, get_random_color()))
+        # cmds.append("4 2 renew %s %s" % (marked_index, get_random_color()))
+        # cmds.append("landslip %s %s %s %s"  % (clicked_index, get_random_color(), marked_index, get_random_color()))
+        cmds.append("landslip %s %s"  % (clicked_index, get_random_color()))
 
         marked_index = -1
+    return cmds, marked_index
 
-def parse_command(command_str):
+def parse_command(command_str, boxs):
     commands = command_str.split(";")
-    cmd_tmp = {}
     animations = []
-    global boxs
     for command in commands:
         params = command.split()
-        animation = Animation(params[2], boxs[int(params[3])])
-        cmd_tmp[params[0]] = animation
-        animation.parent = cmd_tmp.get(params[1])
-        if animation.action == "move":
-            animation.target = (int(params[4]), int(params[5]))
+        action = params[0]
+        if action == "switch":
+            f_index = int(params[1])
+            t_index = int(params[2])
+            animation = Animation("move", boxs[f_index])
+            animation.target = index_to_pos(t_index)
             animations.append(animation)
-        elif animation.action == "renew":
-            animation.target = params[4]
+            animation = Animation("move", boxs[t_index])
+            animation.target = index_to_pos(f_index)
             animations.append(animation)
-        elif animation.action == "landslip":
-            landslips = sorted(zip([int(x) for x in params[3::2]], params[4::2]), reverse=True)
-            parent = animation.parent
+            # boxs[f_index], boxs[t_index] = boxs[t_index], boxs[f_index]
+        elif action == "landslip":
+            landslips = sorted(zip([int(x) for x in params[1::2]], params[2::2]), reverse=True)
+            renew_parent = animations[-1]
+            flag = -1
             for i, (slip_at, new_color) in enumerate(landslips):
                 renew = Animation("renew", boxs[slip_at])
-                renew.target = ((boxs[slip_at].rect.left, (ROWS+len(landslips)-1-i)*BLOCK_SIDE + PADDING/2), new_color)
-                renew.parent = animation.parent
+                renew.target = (new_color, (boxs[slip_at].rect.left, ROWS*BOX_SIDE))
+                renew.parent = renew_parent 
                 animations.append(renew)
+                if col_gap(slip_at, flag) == 0:
+                    move_parent = animations[-2]
+                else:
+                    move_parent = animations[-1]
+                flag = slip_at
+
                 move_animation = Animation("move", boxs[slip_at])
-                for a, b in [(i, i + COLS) for i in range(slip_at, len(boxs) - COLS, COLS)]:
-                    boxs[a], boxs[b] = boxs[b], boxs[a]
-                    move_animation = Animation("move", box)
-                    move_animation.target = (0, -1)
-                    move_animation.parent = parent
+                move_animation.target = (boxs[slip_at].rect.left, (ROWS - 1)*BOX_SIDE)
+                move_animation.parent = move_parent
+                animations.append(move_animation)
+
+                for a, b in [(i, i + COLS) for i in range(slip_at, (ROWS-1)*COLS, COLS)]:
+                    move_animation = Animation("move", boxs[b])
+                    move_animation.target = index_to_pos(a)
+                    move_animation.parent = move_parent
                     animations.append(move_animation)
-                if len(landslips) == 2 and landslips[0] - landslips[1] == COLS:
-                    parent = animations[-1]
-            cmd_tmp[params[0]] = animations[-1]
+                    boxs[a], boxs[b] = boxs[b], boxs[a]
 
     return animations
 
@@ -201,13 +206,13 @@ def run_animations(animations):
             new_animations.append(animation)
             if animation.parent and animation.parent.finished:
                 animation.parent = animation.parent.parent
-    return new_animation    s
+    return new_animations
 
 
 def main():
     # Initialise screen
     pygame.init()
-    screen = pygame.display.set_mode([(BOX_SIDE + PADDING) * COLS,(BOX_SIDE + PADDING) * ROWS])
+    screen = pygame.display.set_mode((BOX_SIDE * COLS, BOX_SIDE * ROWS))
     pygame.display.set_caption("Mirror Mirror")
 
     # Fill background
@@ -215,14 +220,17 @@ def main():
     background = background.convert()
     background.fill(BG_COLOR)
 
-    global boxs, animations,cmds
+    cmds = []
+    mirror_colors=[]
+    animations=[]
+    marked_index = -1
     boxs = generate_boxs()
 
     clock = pygame.time.Clock()
     while 1:
         clock.tick(60)
         if len(animations) == 0 and len(cmds) > 0:
-            animations = parse_command(";".join(cmds))
+            animations = parse_command(";".join(cmds), boxs)
             cmds = []
         if len(animations) > 0:
             animations = run_animations(animations)
@@ -231,10 +239,9 @@ def main():
                 return
             elif event.type == MOUSEBUTTONUP:
                 if len(animations) == 0:
-                    click_at(event.pos)
+                    cmds, marked_index = click_at(event.pos, boxs, marked_index)
         screen.blit(background, (0,0))
         for box in boxs:
-            # box.draw(screen)
             screen.blit(box.image, box.rect)
         if IN_MIRROR:
             mirror = pygame.transform.flip(screen, False, True)
