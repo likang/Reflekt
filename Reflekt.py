4cd1c12ebd016ddadc7213c7cc12ec30623872ce
likang <likang@kisbear.com>
2012-05-16 18:19:53 +0800

    refactor

--- Reflekt.py
+++ Reflekt.py
@@ -4,6 +4,7 @@ import pygame
 import random
 import time
 from pygame.locals import *
+from multiprocessing import Pipe
 
 IMG_SIDE    = 100
 MARK_SIDE   = 20
@@ -66,11 +67,11 @@ class Animation():
                 self.box.image = pygame.transform.scale(self.box.image, (box_side - SCALE_SPEED, )*2)
                 self.box.move((SCALE_SPEED/2, )*2)
             if self.box.image.get_size()[0] == 0:
-                self.finished = True  
                 new_color = self.target[0]
+                new_pos = self.target[1]
+                self.finished = True  
                 self.box.image = get_surface(new_color)
                 self.box.color = new_color
-                new_pos = self.target[1]
                 self.box.move((new_pos[0] - self.box.rect.left, new_pos[1] - self.box.rect.top))
         else:
             self.finished = True
@@ -115,22 +116,17 @@ def pos_to_index(pos):
     index =  pos[0]/BOX_SIDE + COLS*(pos[1]/BOX_SIDE)
     return index if index >= 0 and index < COLS*ROWS else -1
 
-def who_is_at(index, boxs):
-    for box in boxs:
-        if box.rect.topleft == index_to_pos(index):
-            return box
-
 def index_to_pos(index):
     return ((index%COLS)*BOX_SIDE, (index/COLS)*BOX_SIDE)
 
-def row_gap(you, me):
+def row_difference(you, me):
     return you/COLS - me/COLS
 
-def col_gap(you, me):
+def col_difference(you, me):
     return you%COLS - me%COLS
 
 def is_around(you, me):
-    return abs(row_gap(you, me)) + abs(col_gap(you, me)) in (1,0)
+    return abs(row_difference(you, me)) + abs(col_difference(you, me)) in (1,0)
 
 def click_at(pos, boxs, marked_index):
     clicked_index= pos_to_index(pos)
@@ -147,10 +143,7 @@ def click_at(pos, boxs, marked_index):
         boxs[marked_index].mark(False)
 
         cmds.append("switch %s %s" % (clicked_index, marked_index))
-        # cmds.append("3 1 renew %s %s" % (clicked_index, get_random_color()))
-        # cmds.append("4 2 renew %s %s" % (marked_index, get_random_color()))
-        cmds.append("landslip %s %s %s %s"  % (clicked_index, get_random_color(), marked_index, get_random_color()))
-        # cmds.append("landslip %s %s"  % (clicked_index, get_random_color()))
+        cmds.append("landslip %s %s"  % (clicked_index, get_random_color()))
 
         marked_index = -1
     return cmds, marked_index
@@ -164,38 +157,36 @@ def parse_command(command_str, boxs):
         if action == "switch":
             f_index = int(params[1])
             t_index = int(params[2])
-            animation = Animation("move", boxs[f_index])
-            animation.target = boxs[t_index].rect.topleft
-            animations.append(animation)
-            animation = Animation("move", boxs[t_index])
-            animation.target = boxs[f_index].rect.topleft
-            animations.append(animation)
+            move = Animation("move", boxs[f_index])
+            move.target = boxs[t_index].rect.topleft
+            animations.append(move)
+            move = Animation("move", boxs[t_index])
+            move.target = boxs[f_index].rect.topleft
+            animations.append(move)
             boxs[f_index], boxs[t_index] = boxs[t_index], boxs[f_index]
         elif action == "landslip":
             landslips = sorted(zip([int(x) for x in params[1::2]], params[2::2]), reverse=True)
             renew_parent = animations[-1]
             flag = -1
-            for i, (slip_at, new_color) in enumerate(landslips):
+            for slip_at, new_color in landslips:
                 renew = Animation("renew", boxs[slip_at])
                 renew.target = (new_color, (index_to_pos(slip_at)[0], ROWS*BOX_SIDE))
                 renew.parent = renew_parent 
                 animations.append(renew)
-                if col_gap(slip_at, flag) == 0:
-                    move_parent = animations[-2]
-                else:
-                    move_parent = renew
+
+                move_parent = animations[-2] if col_difference(slip_at, flag) == 0 else renew
                 flag = slip_at
 
-                move_animation = Animation("move", boxs[slip_at])
-                move_animation.target = (index_to_pos(slip_at)[0], (ROWS - 1)*BOX_SIDE)
-                move_animation.parent = move_parent
-                animations.append(move_animation)
+                move = Animation("move", boxs[slip_at])
+                move.target = (index_to_pos(slip_at)[0], (ROWS - 1)*BOX_SIDE)
+                move.parent = move_parent
+                animations.append(move)
 
                 for a, b in [(i, i + COLS) for i in range(slip_at, (ROWS-1)*COLS, COLS)]:
-                    move_animation = Animation("move", boxs[b])
-                    move_animation.target = index_to_pos(a)
-                    move_animation.parent = move_parent
-                    animations.append(move_animation)
+                    move = Animation("move", boxs[b])
+                    move.target = index_to_pos(a)
+                    move.parent = move_parent
+                    animations.append(move)
                     boxs[a], boxs[b] = boxs[b], boxs[a]
 
     return animations
@@ -213,7 +204,7 @@ def run_animations(animations):
     return new_animations
 
 
-def main():
+def main(pipe, role):
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((BOX_SIDE * COLS, BOX_SIDE * ROWS))
@@ -234,6 +225,11 @@ def main():
     while 1:
         clock.tick(60)
         if len(animations) == 0 and len(cmds) > 0:
+            if role == "sender":
+                command = ";".join(cmds)
+                pipe.send_bytes(command)
+            else
+                command = pipe.recv_bytes()
             animations = parse_command(";".join(cmds), boxs)
             cmds = []
         if len(animations) > 0:
@@ -253,4 +249,9 @@ def main():
             screen.blit(mirror, (0,0))
         pygame.display.flip()
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': 
+    sender, receiver = Pipe()
+    p = Process(target=main, args=(receiver, "receiver"))
+    p.start()
+    main(sender, "sender")
+    p.join()    
